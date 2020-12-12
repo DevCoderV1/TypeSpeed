5cd56cad7b667f6f19119d34cabdf9da472cc519
M1ck3yZ <49423967+serchman123@users.noreply.github.com>
2020-12-12 17:38:38 -0500

    working on the script

--- script.js
+++ script.js
@@ -2,20 +2,37 @@ const Text = document.getElementById('text')
 const NewText = Text.textContent;
 const SplitText = NewText.split("")
 const UserInput = document.getElementById('userinput')
-const AllSpan = document.querySelectorAll("span");
+const Timer = document.getElementById('timer')
 Text.innerHTML = "";
 
-
-
+//Add span tag to each letter in the text content
 for (let x = 0; x < SplitText.length; x++) {
     Text.innerHTML += "<span>" + SplitText[x] + "</span>"
 }
-let count = 0;
-UserInput.addEventListener('keyup', (e) => {
-    let InputText = e.target.value;
-    count++
-    const Span = text.querySelectorAll('span')[count]
 
-    console.log(Span)
+UserInput.addEventListener('input', (e) => {
+    const Inputtext = e.target.value;
+    const SplitLetter = Inputtext.split("")
+    const SpanText = Text.querySelectorAll("span")
+    let Correct = true
+    SpanText.forEach((letter, index = 12) => {
+        const CurrentLetter = SplitLetter[index]
+        if (CurrentLetter == null) {
+            letter.classList.remove('correctText')
+            letter.classList.remove('incorrectText')
+            Correct = false
+        } else if (CurrentLetter === letter.innerText) {
+            letter.classList.add('correctText')
+            letter.classList.remove('incorrectText')
+        } else {
+            letter.classList.remove('correctText')
+            letter.classList.add('incorrectText')
+            Correct = false
+        }
+    })
+    if (Correct) {
+        console.log(time)
+    }
+
 
 })
