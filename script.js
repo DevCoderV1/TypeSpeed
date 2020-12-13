11d8b44a9ef902e4da8dcfdf7bd9e018ef46df4a
M1ck3yZ <49423967+serchman123@users.noreply.github.com>
2020-12-13 14:28:18 -0500

    re-write code
    added a function that excute on code when hit on "enter" key

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const Story = ["One day a rabbit was boasting about how fast he could run. He was laughing at the turtle for being so slow. Much to the rabbit's surprise, the turtle challenged him to a race. The rabbit thought this was a good joke and accepted the challenge. The fox was to be the umpire of the race. As the race began, the rabbit raced way ahead of the turtle, just like everyone thought.The rabbit got to the halfway point and could not see the turtle anywhere.He was hot and tired and decided to stop and take a short nap. Even if the turtle passed him, he would be able to race to the finish line ahead of him.All this time the turtle kept walking step by step by step.He never quit no matter how hot or tired he got.He just kept going. However, the rabbit slept longer than he had thought and woke up.He could not see the turtle anywhere! He went at full speed to the finish line but found the turtle there waiting for him."]
-
+const textbox = document.querySelector(".type_container")
+const startBTN = document.getElementById("start_btn")
 const Text = document.getElementById('text')
 const SplitText = Story[0].split("")
 const UserInput = document.getElementById('userinput')
@@ -14,17 +15,22 @@ for (let x = 0; x < SplitText.length; x++) {
 //Timer
 let count = 0
 let TimeCount;
-function StartTime() {
-    UserInput.focus()
-    TimeCount = setInterval(function () {
-        // I wrote this on two lines for clarity.
-        count++
+startBTN.focus()
+startBTN.addEventListener('keyup', (e) => {
+    if (e.keyCode === 13) {
+        startBTN.style.display = "none"
+        textbox.style.display = "block"
+        UserInput.focus()
+        TimeCount = setInterval(function () {
+            // I wrote this on two lines for clarity.
+            count++
 
-        Timer.innerHTML = `${count}`
+            Timer.innerHTML = `${count}`
 
-    }, 1000);
+        }, 1000);
+    }
 
-}
+})
 
 //event once enter character in input
 UserInput.addEventListener('input', (e) => {
